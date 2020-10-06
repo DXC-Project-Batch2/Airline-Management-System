06bfa9df623ed14918e6d8ce8eca28040ff864ef
Anilkumar <anil.tony33@gmail.com>
2020-10-06 12:40:22 +0530

    Anil

---
+++ Master/AirlineMangementsystem/src/main/java/com/dxc/airline/AirlineMangementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.dxc.airline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineMangementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineMangementsystemApplication.class, args);
+	}
+
+}
