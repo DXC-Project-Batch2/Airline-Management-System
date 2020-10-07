63fd75281e6dcfe905fea788b5a4014966fe98d3
Indranil27071997 <66701469+Indranil27071997@users.noreply.github.com>
2020-10-07 15:30:48 +0530

    from indranil :)

--- Master/AirlineMangementsystem/src/main/java/com/dxc/airline/model/Admin.java
+++ Master/AirlineMangementsystem/src/main/java/com/dxc/airline/model/Admin.java
@@ -7,55 +7,54 @@ import org.springframework.stereotype.Component;
 
 @Entity
 @Component
-public class User {
+public class Admin {
 	
 	@Id
-	
 	int id;
 	String username;
 	String password;
 	
 	
-	public User() {
+	public Admin() {
 		super();
 		
 	}
 	
-	public User(int id, String username, String password) {
+	
+	
+	public Admin(int id, String username, String password) {
 		super();
 		this.id = id;
 		this.username = username;
 		this.password = password;
 	}
-
+	
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+
+
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
+		return "Admin [id=" + id + ", username=" + username + ", password=" + password + "]";
 	}
+	
 	
 	
 	
