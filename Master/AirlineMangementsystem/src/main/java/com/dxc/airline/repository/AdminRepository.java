0bd76f95ec08780e8f13ca887679500b47293dd7
rbathina <rbathina@IN-5CG0251NV6>
2020-10-08 12:04:12 +0530

    By Rama krishna

--- Master/AirlineMangementsystem/src/main/java/com/dxc/airline/repository/AdminRepository.java
+++ Master/AirlineMangementsystem/src/main/java/com/dxc/airline/repository/AdminRepository.java
@@ -1,9 +1,10 @@
 package com.dxc.airline.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.dxc.airline.model.Admin;
 
-public interface AdminRepository extends CrudRepository<Admin, Integer> {
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
 
 }
--- Master/AirlineMangementsystem/src/main/java/com/dxc/airline/repository/AirLineRepository.java
+++ Master/AirlineMangementsystem/src/main/java/com/dxc/airline/repository/AirLineRepository.java
@@ -1,9 +1,10 @@
 package com.dxc.airline.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.dxc.airline.model.AirLine;
 
-public interface AirLineRepository extends CrudRepository<AirLine, Integer> {
+public interface AirLineRepository extends JpaRepository<AirLine, Integer> {
 
 }
