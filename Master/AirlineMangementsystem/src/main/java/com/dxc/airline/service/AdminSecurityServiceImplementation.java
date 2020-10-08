38f5ccb984cb5f1cd036af4ca05bcdbb3669b0fc
paduri2 <paduri2@host.docker.internal>
2020-10-08 21:16:39 +0530

    check by pasupathi

---
+++ Master/AirlineMangementsystem/src/main/java/com/dxc/airline/service/AdminSecurityServiceImplementation.java
@@ -0,0 +1,51 @@
+package com.dxc.airline.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.dxc.airline.model.AdminSecurity;
+import com.dxc.airline.repository.AdminSecurityRepository;
+
+public class AdminSecurityServiceImplementation  implements AdminSecurityService<AdminSecurity> {
+
+	@Autowired
+	AdminSecurityRepository adminSecurityRepository;
+	
+	@Override
+	public boolean save(AdminSecurity e) {
+
+		boolean res=false;
+		if(adminSecurityRepository.save(e) != null)
+		{
+			res=true;
+		}
+		return res;
+	}
+
+	@Override
+	public AdminSecurity find(long id) {
+		Optional<AdminSecurity> adminInfo = adminSecurityRepository.findById(id);
+		return adminInfo.get();
+	}
+
+	@Override
+	public List<AdminSecurity> findAll() {
+		return adminSecurityRepository.findAll();
+	}
+
+	@Override
+	public boolean update(AdminSecurity e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(long id) {
+		// TODO Auto-generated method stub
+		adminSecurityRepository.deleteById(id);
+		return true;
+	}
+
+}
