c787195c1c9f92573e413e15645b780819775889
paduri2 <paduri2@host.docker.internal>
2020-10-08 21:11:14 +0530

    by pasupathi

--- Master/AirlineMangementsystem/src/main/java/com/dxc/airline/repository/AdminSecurityRepository.java
+++ Master/AirlineMangementsystem/src/main/java/com/dxc/airline/repository/AdminSecurityRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dxc.airline.model.AdminSecurity;
 
-public interface AdminSecurityRepository extends JpaRepository<AdminSecurity , String >{
+public interface AdminSecurityRepository extends JpaRepository<AdminSecurity , Long >{
 
 }
