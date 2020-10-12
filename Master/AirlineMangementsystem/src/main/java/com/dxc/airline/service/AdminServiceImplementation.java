abbf0a53f40c58a512d5d361db04b330803bda05
Indranil27071997 <66701469+Indranil27071997@users.noreply.github.com>
2020-10-12 16:41:38 +0530

    hi

--- Master/AirlineMangementsystem/src/main/java/com/dxc/airline/service/AdminServiceImplementation.java
+++ Master/AirlineMangementsystem/src/main/java/com/dxc/airline/service/AdminServiceImplementation.java
@@ -18,6 +18,8 @@ public class AdminServiceImplementation implements AdminService<Admin> {
 	@Override
 	public List<Admin> findAll() {
 		// TODO Auto-generated method stub
+		
+		
 		return adminRepository.findAll();
 	}
 
@@ -29,6 +31,8 @@ public class AdminServiceImplementation implements AdminService<Admin> {
 		{
 			res = true;
 		}
+		
+		
 		return res;
 	}
 
