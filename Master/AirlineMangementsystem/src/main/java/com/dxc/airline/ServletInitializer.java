a87f35d4109eecc8ac89380e8e1cb6af30c683f6
paduri2 <paduri2@host.docker.internal>
2020-10-13 14:43:45 +0530

    cleared all errors

--- Master/AirlineMangementsystem/src/main/java/com/dxc/airline/ServletInitializer.java
+++ Master/AirlineMangementsystem/src/main/java/com/dxc/airline/ServletInitializer.java
@@ -3,7 +3,7 @@ package com.dxc.airline;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+public class ServletInitializer extends SpringBootServletInitializer{
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
