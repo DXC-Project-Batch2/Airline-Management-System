9b5d9e7a98171d810d1ffa2179ea5af6e0de4fa2
paduri2 <paduri2@host.docker.internal>
2020-10-15 18:51:19 +0530

    by pasupathi

--- Master/AirlineMangementsystem/src/main/java/com/dxc/airline/model/Registration.java
+++ Master/AirlineMangementsystem/src/main/java/com/dxc/airline/model/Registration.java
@@ -1,116 +1,244 @@
 package com.dxc.airline.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import org.springframework.stereotype.Component;
-
-@Component
-@Entity
 public class Registration {
 	
-	@Id
-	private long mobileNumber;
-	private String mailId;
-	private String firstName;
-	private String lastName;
-	private String fatherName;
-	private long DOB;
-	private String passportNumber;
-	private String addressLine1;
-	private String adrressLine2;
-	private String state;
-	private long postalcode;
-	private String country;
-	private String password;
-	private String conformPassword;
-	public long getMobileNumber() {
-		return mobileNumber;
+	String username;
+	String password;
+	String name;
+	String Lastname;
+	String gender;
+	Date dob;
+	String fathername;
+	String passportname;
+	long passportnumber;
+	Date expiry;
+	String doorNumber;
+	String street;
+	String area;
+	String country;
+	String state;
+	String city;
+	long postalCode;
+	String landMark;
+	String mobile; 
+	String securityQuestion;
+	String answer;
+	
+	public Registration() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
-	public void setMobileNumber(long mobileNumber) {
-		this.mobileNumber = mobileNumber;
-	}
-	public String getMailId() {
-		return mailId;
-	}
-	public void setMailId(String mailId) {
-		this.mailId = mailId;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getFatherName() {
-		return fatherName;
-	}
-	public void setFatherName(String fatherName) {
-		this.fatherName = fatherName;
-	}
-	public long getDOB() {
-		return DOB;
-	}
-	public void setDOB(long dOB) {
-		DOB = dOB;
-	}
-	public String getPassportNumber() {
-		return passportNumber;
-	}
-	public void setPassportNumber(String passportNumber) {
-		this.passportNumber = passportNumber;
-	}
-	public String getAddressLine1() {
-		return addressLine1;
-	}
-	public void setAddressLine1(String addressLine1) {
-		this.addressLine1 = addressLine1;
-	}
-	public String getAdrressLine2() {
-		return adrressLine2;
-	}
-	public void setAdrressLine2(String adrressLine2) {
-		this.adrressLine2 = adrressLine2;
-	}
-	public String getState() {
-		return state;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	public long getPostalcode() {
-		return postalcode;
-	}
-	public void setPostalcode(long postalcode) {
-		this.postalcode = postalcode;
-	}
-	public String getCountry() {
-		return country;
-	}
-	public void setCountry(String country) {
+
+	public Registration(String username, String password, String name, String lastname, String gender, String strdob,
+			String fathername, String passportname, long passportnumber, String strexpiry, String doorNumber, String street,
+			String area, String country, String state, String city, long postalCode, String landMark, String mobile,
+			String securityQuestion, String answer) throws ParseException {
+		
+		super();
+		this.username = username;
+		this.password = password;
+		this.name = name;
+		Lastname = lastname;
+		this.gender = gender;
+		SimpleDateFormat sdf=new SimpleDateFormat("dd-MM-yyyy");
+		dob=sdf.parse(strdob);
+		this.fathername = fathername;
+		this.passportname = passportname;
+		this.passportnumber = passportnumber;
+		expiry = sdf.parse(strexpiry);
+		this.doorNumber = doorNumber;
+		this.street = street;
+		this.area = area;
 		this.country = country;
+		this.state = state;
+		this.city = city;
+		this.postalCode = postalCode;
+		this.landMark = landMark;
+		this.mobile = mobile;
+		this.securityQuestion = securityQuestion;
+		this.answer = answer;
 	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public String getConformPassword() {
-		return conformPassword;
-	}
-	public void setConformPassword(String conformPassword) {
-		this.conformPassword = conformPassword;
-	}
-	
-	
-	
-	
 
-}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastname() {
+		return Lastname;
+	}
+
+	public void setLastname(String lastname) {
+		Lastname = lastname;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getFathername() {
+		return fathername;
+	}
+
+	public void setFathername(String fathername) {
+		this.fathername = fathername;
+	}
+
+	public String getPassportname() {
+		return passportname;
+	}
+
+	public void setPassportname(String passportname) {
+		this.passportname = passportname;
+	}
+
+	public long getPassportnumber() {
+		return passportnumber;
+	}
+
+	public void setPassportnumber(long passportnumber) {
+		this.passportnumber = passportnumber;
+	}
+
+	public Date getExpiry() {
+		return expiry;
+	}
+
+	public void setExpiry(Date expiry) {
+		this.expiry = expiry;
+	}
+
+	public String getDoorNumber() {
+		return doorNumber;
+	}
+
+	public void setDoorNumber(String doorNumber) {
+		this.doorNumber = doorNumber;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public long getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(long postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getLandMark() {
+		return landMark;
+	}
+
+	public void setLandMark(String landMark) {
+		this.landMark = landMark;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getSecurityQuestion() {
+		return securityQuestion;
+	}
+
+	public void setSecurityQuestion(String securityQuestion) {
+		this.securityQuestion = securityQuestion;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+
+	@Override
+	public String toString() {
+		return "Registration [username=" + username + ", password=" + password + ", name=" + name + ", Lastname="
+				+ Lastname + ", gender=" + gender + ", dob=" + dob + ", fathername=" + fathername + ", passportname="
+				+ passportname + ", passportnumber=" + passportnumber + ", expiry=" + expiry + ", doorNumber="
+				+ doorNumber + ", street=" + street + ", area=" + area + ", country=" + country + ", state=" + state
+				+ ", city=" + city + ", postalCode=" + postalCode + ", landMark=" + landMark + ", mobile=" + mobile
+				+ ", securityQuestion=" + securityQuestion + ", answer=" + answer + "]";
+	}
+	
+	}
